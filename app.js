db2b3f4189391fa8b003babaa6b09dad68c336d6
chetan <chetan@ubuntu.(none)>
2014-09-29 00:15:25 +0530

    Added Request Module

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 var MailListener = require("mail-listener2");
+var request = require("request");
+
+var options = {
+  url : 'https://api.built.io/v1/classes/tickets/objects/',
+  headers : {
+    'application_api_key' : 'bltc3c58bccdff3fcc7',
+    'application_uid' : 'support_ticket',
+    'content-type' : 'application/json'
+  }
+};
+
+var message = {};
 
 var mailListener = new MailListener({
   username: "builtflow@gmail.com",
@@ -35,9 +47,27 @@ mailListener.on("error", function(err){
 
 mailListener.on("mail", function(mail, seqno, attributes){
   // do something with mail object including attachments
-  console.log("Received Mail From : ", mail.headers.from);
+  /*console.log("Received Mail From : ", mail.headers.from);
   console.log("Subject : ", mail.headers.subject);
-  console.log("-x-x-x-");
+  console.log(Object.keys(mail));*/
+
+  message.sender_name = mail.from[0].name;
+  message.sender_email = mail.from[0].address;
+  message.subject = mail.subject;
+  message.message = mail.html;
+
+  request.post(options, 
+    function(error, res, body){
+      // console.log("Error : ", error);
+      // console.log("Response : ", res);
+
+      var ack = JSON.parse(body);
+      console.log("Body : ", ack.object.uid);
+  })
+  .form({object:message});
+
+  console.log( JSON.stringify(message) );
+  console.log("-x-x-x-"); 
   // mail processing code goes here
 });
 
